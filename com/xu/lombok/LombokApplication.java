36eb1fc673949ccf825ee75648ddc23fac586d61
guanxu <1510424541@qq.com>
2025-02-12 18:55:49 +0800

    feat: project init

---
+++ com/xu/lombok/LombokApplication.java
@@ -0,0 +1,17 @@
+package com.xu.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application 启动类
+ *
+ * @author xuguan
+ * @since 2025/2/12
+ */
+@SpringBootApplication
+public class LombokApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LombokApplication.class, args);
+    }
+}
